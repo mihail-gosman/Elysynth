61391e532f8edec75558151085c07d68d1bc0cd1
mihail-gosman <mihailc.gosman@gmail.com>
2025-04-02 11:20:46 +0300

    Refactor UI for simpler and cleaner project display

--- ElysynthGUI/Form1.cs
+++ ElysynthGUI/Form1.cs
@@ -13,12 +13,12 @@ namespace ElysynthGUI
         private Management.ProjectHandler ProjectHandler = new ProjectHandler();
         private Management.SettingsHandler SettingsHandler = new SettingsHandler();
 
-        private Label _lblProject;
+        private Label _lblProjects;
         private Label[] _lblsProjects;
 
-        private const int LATIME_CONTROL = 100;
-        private const int DIMENSIUNE_PAS_Y = 30;
-        private const int DIMENSIUNE_PAS_X = 120;
+        private const int CONTROL_WIDTH = 120;
+        private const int Y_OFFSET = 30;
+        private const int X_OFFSET = 150;
 
         public Form1()
         {
@@ -26,20 +26,24 @@ namespace ElysynthGUI
             ProjectHandler.SetPath(null);
             SettingsHandler.SetPath(null);
 
-            
-            this.Size = new Size(500, 300); 
-            this.StartPosition = FormStartPosition.Manual;
-            this.Location = new Point(110, 100);
-            this.Font = new Font("Arial", 9, FontStyle.Bold);
-            this.ForeColor = Color.LimeGreen;
+            // Window Settings
+            this.Size = new Size(500, 300);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Font = new Font("Arial", 9, FontStyle.Regular);
+            this.ForeColor = Color.Black;
             this.Text = "Elysynth";
 
-            _lblProject = new Label();
-            _lblProject.Width = LATIME_CONTROL;
-            _lblProject.Text = "Projects";
-            _lblProject.Left = DIMENSIUNE_PAS_X;
-            _lblProject.ForeColor = Color.DarkGreen;
-            this.Controls.Add(_lblProject);
+            // Setup "Projects" label
+            _lblProjects = new Label
+            {
+                Width = CONTROL_WIDTH,
+                Text = "Projects:",
+                Left = X_OFFSET,
+                Top = 20,
+                ForeColor = Color.Black,
+                Font = new Font("Arial", 10, FontStyle.Bold)
+            };
+            this.Controls.Add(_lblProjects);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -50,23 +54,24 @@ namespace ElysynthGUI
         private void ShowProjects()
         {
             string[] projectNames = ProjectHandler.GetProjectsNames();
-
-            int yOffset = DIMENSIUNE_PAS_Y; 
+            int yOffset = Y_OFFSET + 20;
 
             _lblsProjects = new Label[projectNames.Length];
 
             for (int i = 0; i < projectNames.Length; i++)
             {
-                
-                _lblsProjects[i] = new Label();
-                _lblsProjects[i].Width = 400;
-                _lblsProjects[i].Text = Path.GetFileName(projectNames[i]);
-                _lblsProjects[i].Left = DIMENSIUNE_PAS_X + LATIME_CONTROL; 
-                _lblsProjects[i].Top = yOffset;
-                _lblsProjects[i].ForeColor = Color.DarkGreen;
+                _lblsProjects[i] = new Label
+                {
+                    Width = 350,
+                    Text = Path.GetFileName(projectNames[i]),
+                    Left = X_OFFSET + CONTROL_WIDTH,
+                    Top = yOffset,
+                    ForeColor = Color.Black,
+                    Font = new Font("Arial", 9, FontStyle.Regular)
+                };
                 this.Controls.Add(_lblsProjects[i]);
 
-                yOffset += DIMENSIUNE_PAS_Y;
+                yOffset += Y_OFFSET;
             }
         }
     }
